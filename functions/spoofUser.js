5f5faf4d2cf89ed42dafb4556a4a33eb4ee8c1dd
Nix <contato@lobotomy.space>
2025-04-16 14:43:33 -0300

    [☹️] I don't like this feature.

--- functions/spoofUser.js
+++ functions/spoofUser.js
@@ -5,6 +5,6 @@ plppdo.on('domChanged', () => {
         const desiredUsername = featureConfigs.customUsername || user.nickname;
 
         if (nicknameElement && nicknameElement.textContent !== desiredUsername) nicknameElement.textContent = desiredUsername;
-        //if (featureConfigs.customPfp && pfpElement) { if (pfpElement.src !== featureConfigs.customPfp) { Object.assign(pfpElement, { src: featureConfigs.customPfp, alt: "Not an image URL" }); pfpElement.style.borderRadius = "50%"; }}
+        if (featureConfigs.customPfp && pfpElement) { if (pfpElement.src !== featureConfigs.customPfp) { Object.assign(pfpElement, { src: featureConfigs.customPfp, alt: "Not an image URL" }); pfpElement.style.borderRadius = "50%"; }}
     }
 });
